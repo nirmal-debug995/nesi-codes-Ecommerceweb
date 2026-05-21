57b5f83d2c9a067dec67c2163e1f006cf7dd14f1
Ubuntu <azureuser@app-dev-vm.xvispsaiqybutnuu420jilhqie.rx.internal.cloudapp.net>
2026-05-21 06:40:49 +0000

    Add frontend GitHub Actions workflow

--- 03-frontend/angular-ecommerce/src/environments/environment.prod.ts
+++ 03-frontend/angular-ecommerce/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://98.70.45.172:8080/api'
+  apiUrl: '/api'
 };
--- 03-frontend/angular-ecommerce/src/environments/environment.ts
+++ 03-frontend/angular-ecommerce/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://98.70.45.172:8080/api'
+  apiUrl: '/api'
 };
